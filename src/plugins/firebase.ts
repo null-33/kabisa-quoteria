8f5de3facb7d2fe048550dc8ad3368cb32149f68
David Hoevers <david@manklavier.nl>
2021-06-19 15:51:02 +0200

    use custom linting rules and fix issues add firebase

---
+++ src/plugins/firebase.ts
@@ -0,0 +1,13 @@
+import firebase from 'firebase';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyDqo4efGOvkxUKejYNReVVghkZIIgc5jVk',
+  authDomain: 'kabisa-quoteria.firebaseapp.com',
+  databaseURL: 'https://kabisa-quoteria-default-rtdb.europe-west1.firebasedatabase.app',
+  projectId: 'kabisa-quoteria',
+  storageBucket: 'kabisa-quoteria.appspot.com',
+  messagingSenderId: '301453449889',
+  appId: '1:301453449889:web:075307353996ece110aee0',
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
